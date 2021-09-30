972936d909018e39574744eb055e0e89c9c128b3
V. Bruk <vbruk@hey.com>
2021-09-30 16:44:58 -0300

    Add teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :teachers
   root to: 'pages#index'
+
+  namespace :teachers do
+    # teachers roots
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
