dcc0ca86580d6a63aac6a4c3de4bcb381adc497d
V. Bruk <vbruk@hey.com>
2021-09-30 16:46:34 -0300

    Add students

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :students
   devise_for :teachers
   root to: 'pages#index'
 
   namespace :teachers do
     # teachers roots
   end
+
+  namespace :students do
+    # students roots
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
