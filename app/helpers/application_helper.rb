3fc9faab1aa8c03c84059b391c37ce428faa4d07
V. Bruk <vbruk@hey.com>
2021-10-11 12:23:55 -0300

    Frontend setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def seo_title(page_title = '')
+    base_title = "Tutores 21"
+    if page_title.empty?
+      base_title
+    else
+      [page_title, base_title].join(" | ")
+    end
+  end
+
+  # def bootstrap_class_for flash_type
+  #   { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  # end
+
+  # def flash_messages(opts = {})
+  #   flash.each do |msg_type, message|
+  #     concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show text-center", role: "alert") do
+  #         concat message
+  #         concat content_tag(:button, '', class: "btn-close", data: { 'bs-dismiss': 'alert' })
+  #       end)
+  #   end
+  #   nil
+  # end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,23 @@ module Tutores21
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.time_zone = 'Buenos Aires'
+
+    # I18n config
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en]
+    config.i18n.fallbacks = [:en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    config.autoload_paths += Dir[Rails.root.join('app', 'classes', '*/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '*/')]
+
+    # ActiveJobs
+    config.active_job.queue_adapter = :sidekiq
+
+    # For replacing ActiveStorage unique attachments
+    config.active_storage.replace_on_assign_to_many = true
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
